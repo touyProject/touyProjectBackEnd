0cb615239e0bdce59fc3db94c84562c1a16aa9af
BHC-Chicken <tij05078@gmail.com>
2023-07-31 17:43:49 +0900

    chore : add tag validation

--- src/main/java/dev/ioexception/touyProjectBackEnd/util/RegexValidation.java
+++ src/main/java/dev/ioexception/touyProjectBackEnd/util/RegexValidation.java
@@ -7,19 +7,22 @@ public class RegexValidation {
     String emailRegex = "^[A-Za-z0-9]+@[A-Za-z0-9]+\\.+ac\\.kr$";
     String nicknameRegex = "^[a-zA-Z0-9가-힣]{3,10}$";
     String passwordRegex = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$";
+    String TAG_REGEX = "^[a-z0-9가-힣]{1,10}$";
 
 
     public boolean isEmailRegex(String email) {
-
         return email.matches(emailRegex);
     }
 
-    public boolean isNicknameRegex(String nickname)
-    {
+    public boolean isNicknameRegex(String nickname) {
         return nickname.matches(nicknameRegex);
     }
 
     public boolean isPasswordRegex(String password) {
         return password.matches(passwordRegex);
+    }
+
+    public boolean isTagRex(String tag) {
+        return tag.matches(TAG_REGEX);
     }
 }
